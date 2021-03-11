e0dec1136f1a31a34b164d917115577940b4240c
103311242 <103311242@student.swin.edu.au>
2021-03-11 17:03:54 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Selection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your username?");
+            int 
+            string userName = Console.ReadLine();
+            if 
+        }
+    }
+}
